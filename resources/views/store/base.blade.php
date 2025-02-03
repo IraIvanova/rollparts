38cc372b9bc0fad3b3968153c094ed27290f8448
iryna.ivanova <avonavis@gmail.com>
2025-02-03 11:47:17 +0200

    remove extra styles

--- resources/views/store/base.blade.php
+++ resources/views/store/base.blade.php
@@ -10,8 +10,8 @@
     <link rel="stylesheet" media="all" href="{{asset('css/store/bootstrap.min.css')}}">
     <link rel="stylesheet" media="all" href="{{asset('css/store/styles.css')}}">
 
-    <link type="text/css" rel="stylesheet" href="{{asset('css/slick.css')}}"/>
-    <link type="text/css" rel="stylesheet" href="{{asset('css/slick-theme.css')}}"/>
+    <link type="text/css" rel="stylesheet" href="{{asset('css/store/slick.css')}}"/>
+    <link type="text/css" rel="stylesheet" href="{{asset('css/store/slick-theme.css')}}"/>
 
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
             integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
